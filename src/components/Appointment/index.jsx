6a58b876fad1cdf5989a869cc63e9d3837a9460b
Ahmed Tarabia <ahmedtarabia@gmail.com>
2021-10-28 17:01:19 -0400

    format files

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,15 +1,17 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 import Header from "./Header.jsx";
 import Show from "./Show.jsx";
 import Empty from "./Empty.jsx";
 
 import "components/Appointment/styles.scss"
 
-export default function Appointment (props) {
+export default function Appointment(props) {
   return (
     <Fragment>
-    <header>{props.time}</header>
-    {props.interview ? <article className="appointment"><Show interviewer={props.interview.interviewer.name} student={props.interview.student} /></article> : <article className="appointment"><Empty /></article>}
+      <header>{props.time}</header>
+      {props.interview ? <article className="appointment">
+        <Show interviewer={props.interview.interviewer.name} student={props.interview.student} /></article>
+        : <article className="appointment"><Empty /></article>}
     </Fragment>
   );
 }
